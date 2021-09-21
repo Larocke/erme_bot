27de7a8f5ad7ec17e1bf24b72055a52563c05749
John Grigoryev <58956151+Larocke@users.noreply.github.com>
2021-09-21 18:02:50 -0400

    added a method to navigate pages

--- app.py
+++ app.py
@@ -1,5 +1,8 @@
 import time
 from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
 browser = webdriver.Chrome('/Users/kat/Desktop/Python/ERME-bot/chromedriver')
 
@@ -10,6 +13,19 @@ browser = webdriver.Chrome('/Users/kat/Desktop/Python/ERME-bot/chromedriver')
 browser.get('https://www.bestbuy.com/site/sony-playstation-5-dualsense-wireless-controller-midnight-black/6464307.p?skuId=6464307')
 
 buy_button = False
+
+def go_to(class_name):
+    try:
+        element = WebDriverWait(browser, 30).until(
+            EC.element_to_be_clickable((By.CLASS_NAME, class_name))
+        )
+        go_to_cart_btn = browser.find_element_by_class_name(class_name)
+        go_to_cart_btn.click()
+        print('Went to', class_name)
+    except:
+        browser.quit()
+        print('Quit because was not able get to', class_name)
+
 
 while not buy_button:
     try:
@@ -26,11 +42,14 @@ while not buy_button:
         
         add_to_cart_btn = browser.find_element_by_class_name('c-button-primary')
         add_to_cart_btn.click()
-        
         print('Added to cart')
 
-        browser.get('https://www.bestbuy.com/cart')
-        
-        print('Went to cart')
+        go_to('go-to-cart-button')
+        go_to('checkout-buttons__checkout')
+        go_to('cia-guest-content')
 
-        browser.quit()
+        
+
+        
+        
+
